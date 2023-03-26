4bd195492c6d77f4e7314eb65c6ddd543ee17b53
ariajd27 <122955040+ariajd27@users.noreply.github.com>
2023-03-25 23:04:04 -0400

    Add files via upload

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,116 @@
+﻿using MonteCarlo;
+using System;
+using System.Linq;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool noai = args.Any(s => s == "noai");
+            bool aionly = args.Any(s => s == "aionly");
+            if (noai && aionly)
+            {
+                Console.WriteLine("That doesn't make any sense, how can a game have no AI but be AI only?");
+                Environment.Exit(1);
+            }
+
+            bool lesscalc = args.Any(s => s == "lesscalc");
+            bool morecalc = args.Any(s => s == "morecalc");
+            if (lesscalc && morecalc)
+            {
+                Console.WriteLine("That doesn't make any sense, how can there be both less and more calculation?");
+                Environment.Exit(1);
+            }
+            int thoroughness = lesscalc ? 5000 
+                : morecalc ? 50000 
+                : 20000;
+
+            var game = new TicTacToeState();
+
+            if (!aionly)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Greetings, human!");
+                Console.WriteLine();
+                Console.WriteLine("The format for identifying spaces is four digits, 0 - 2, representing:");
+                Console.WriteLine("1. number of columns down from top the relevant mini-board is,");
+                Console.WriteLine("2. number of columns across from left the relevant mini-board is,");
+                Console.WriteLine("3. number of columns down from top the space on the relevant mini-board is, and");
+                Console.WriteLine("4. number of columns across from left the space on the relevant mini-board is.");
+                Console.WriteLine("For example, the top-left corner of the full board is 0000, and the center is 1111.");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Current player: {game.CurrentPlayer}");
+            Console.WriteLine();
+            Console.WriteLine(game);
+
+            while (game.Actions.Any())
+            {
+                int position;
+                if (!aionly)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Choose a free space.");
+                    position = -1;
+                    while (true)
+                    {
+                        var input = Console.ReadLine();
+                        if (input == null || input.Length != 4 || input.Any(c => c != '0' && c != '1' && c != '2'))
+                        {
+                            Console.WriteLine("This input could not be parsed. Please try again.");
+                            continue;
+                        }
+
+                        position = 0;
+                        for (int i = 0; i < 4; i++)
+                        {
+                            position *= 3;
+                            position += int.Parse(input[i].ToString());
+                        }
+                        break;
+                    }
+                    Console.WriteLine();
+                    game.ApplyAction(new TicTacToeAction(position, TicTacToePlayer.X));
+                }
+
+                if (!noai)
+                {
+                    var computer = MonteCarloTreeSearch.GetTopActions(game, thoroughness).ToList();
+                    Console.WriteLine();
+                    if (computer.Count > 0)
+                    {
+                        //Console.WriteLine("Computer's ranked plays:");
+                        //foreach (var a in computer)
+                        //    Console.WriteLine($"\t{a.Action}\t{a.NumWins}/{a.NumRuns} ({a.NumWins / a.NumRuns})");
+                        game.ApplyAction(computer[0].Action);
+                    }
+
+                    position = -1;
+                }
+
+                Console.Clear();
+                if (game.Actions.Any())
+                {
+                    Console.WriteLine($"Current player: {game.CurrentPlayer}");
+                }
+                else if (game.GetResult(TicTacToePlayer.X) != 0.5)
+                {
+                    string winner = game.GetResult(TicTacToePlayer.X) == 1 ? "X" : "O";
+                    Console.WriteLine($"{winner} has won!");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Interestingly, the game seems to have ended in a stalemate.");
+                    Console.WriteLine("How strange.");
+                    Environment.Exit(0);
+                }
+                Console.WriteLine();
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,191 @@
+﻿using MonteCarlo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class TicTacToePlayer : IPlayer
+    {
+        public bool IsX { get; }
+
+        private TicTacToePlayer (bool isX)
+        {
+            IsX = isX;
+        }
+
+        public TicTacToePlayer NextPlayer => IsX ? O : X;
+        public TicTacToePlayer LastPlayer => IsX ? O : X;
+
+        public static TicTacToePlayer X = new TicTacToePlayer(true);
+        public static TicTacToePlayer O = new TicTacToePlayer(false);
+
+        public override string ToString()
+        {
+            return IsX ? "X" : "O";
+        }
+    }
+    
+    public struct TicTacToeAction : IAction
+    {
+        public int Position { get; private set; }
+        public TicTacToePlayer Player { get; private set; }
+
+        public TicTacToeAction(int position, TicTacToePlayer player)
+        {
+            if (position < 0 || position > 80) throw new ArgumentException("all items in position must be between 0 and 80, inclusive", nameof(position));
+
+            Position = position;
+            Player = player;
+        }
+
+        public override string ToString()
+        {
+            string frontendPosition = (Position / 27).ToString() 
+                + (Position % 27 / 9).ToString()
+                + (Position % 9 / 3).ToString()
+                + (Position % 3).ToString();
+            return $"{Player}@{frontendPosition}";
+        }
+    }
+
+    public class TicTacToeState : IState<TicTacToePlayer, TicTacToeAction>
+    {
+        private IList<TicTacToePlayer> board;
+        private int lastMovePosition;
+        public TicTacToePlayer CurrentPlayer { get; private set; }
+
+        private static int[][] winningCombos = new[]
+        {
+            new[] { 0, 1, 2 },
+            new[] { 0, 4, 8 },
+            new[] { 0, 3, 6 },
+            new[] { 1, 4, 7 },
+            new[] { 2, 4, 6 },
+            new[] { 2, 5, 8 },
+            new[] { 3, 4, 5 },
+            new[] { 6, 7, 8 }
+        };
+
+        public TicTacToeState(IList<TicTacToePlayer> board, int lastMovePosition, TicTacToePlayer currentPlayer)
+        {
+            this.board = board;
+            this.lastMovePosition = lastMovePosition;
+            CurrentPlayer = currentPlayer;
+        }
+
+        public TicTacToeState() : this(new TicTacToePlayer[81], -1, TicTacToePlayer.X) { } 
+
+        private bool MiniHasWinner (int miniBoard, TicTacToePlayer forPlayer)
+        {
+            return winningCombos.Any(c => c.All(i => board[9 * miniBoard + i] != null && board[9 * miniBoard + i].IsX == forPlayer.IsX));
+        }
+        private bool HasWinner (TicTacToePlayer forPlayer)
+        {
+            TicTacToePlayer[] bigBoard = new TicTacToePlayer[9];
+            for (int i = 0; i < 9; i++)
+            {
+                if (MiniHasWinner(i, forPlayer)) bigBoard[i] = TicTacToePlayer.X;
+            }
+
+            return winningCombos.Any(c => c.All(i => bigBoard[i] != null && bigBoard[i].IsX == forPlayer.IsX));
+        }
+        private bool IsFinished (int miniBoard)
+        {
+            if (MiniHasWinner(miniBoard, TicTacToePlayer.X) || MiniHasWinner(miniBoard, TicTacToePlayer.O)) return true;
+            return board.Take(9 + miniBoard * 9).TakeLast(9).All(c => c != null);
+        }
+
+        public double GetResult(TicTacToePlayer forPlayer)
+        {
+            return HasWinner(forPlayer) ? 1 
+                : HasWinner(forPlayer.NextPlayer) ? 0 
+                : 0.5;
+        }
+
+        public IList<TicTacToeAction> Actions
+        {
+            get
+            {
+                // there are no valid actions from a game over state
+                if (HasWinner(TicTacToePlayer.X) || HasWinner(TicTacToePlayer.O))
+                {
+                    return new TicTacToeAction[0];
+                }
+
+                // playing on the corresponding mini board is mandatory if possible
+                if (lastMovePosition != -1 && !IsFinished(lastMovePosition))
+                {
+                    return board.Take(9 + lastMovePosition * 9).TakeLast(9)
+                        .Select((player, position) => new { player, position })
+                        .Where(o => o.player == null)
+                        .Select((o) => new TicTacToeAction(9 * lastMovePosition + o.position, CurrentPlayer))
+                        .ToList();
+                }
+
+                // free placement if that board's done, though! or if it's the first move
+                return board.Take(81)
+                    .Select((player, position) => new { player, position })
+                    .Where(o => o.player == null)
+                    .Select((o) => new TicTacToeAction(o.position, CurrentPlayer))
+                    .ToList();
+            }
+        }
+
+        public void ApplyAction(TicTacToeAction action)
+        {
+            board[action.Position] = action.Player;
+            lastMovePosition = action.Position % 9;
+            CurrentPlayer = CurrentPlayer.NextPlayer;
+        }
+
+        public IState<TicTacToePlayer, TicTacToeAction> Clone()
+        {
+            return new TicTacToeState(board.ToArray(), lastMovePosition, CurrentPlayer);
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            int[] posCorrespondences = new int[]
+            {
+                0, 1, 2, -1, 9, 10, 11, -1, 18, 19, 20, -4,
+                3, 4, 5, -1, 12, 13, 14, -1, 21, 22, 23, -4,
+                6, 7, 8, -1, 15, 16, 17, -1, 24, 25, 26, -4,
+                -2, -2, -2, -3, -2, -2, -2, -3, -2, -2, -2, -4,
+                27, 28, 29, -1, 36, 37, 38, -1, 45, 46, 47, -4,
+                30, 31, 32, -1, 39, 40, 41, -1, 48, 49, 50, -4,
+                33, 34, 35, -1, 42, 43, 44, -1, 51, 52, 53, -4,
+                -2, -2, -2, -3, -2, -2, -2, -3, -2, -2, -2, -4,
+                54, 55, 56, -1, 63, 64, 65, -1, 72, 73, 74, -4,
+                57, 58, 59, -1, 66, 67, 68, -1, 75, 76, 77, -4,
+                60, 61, 62, -1, 69, 70, 71, -1, 78, 79, 80
+            };
+            for (int i = 0; i < posCorrespondences.Length; i++)
+            {
+                if (posCorrespondences[i] >= 0)
+                {
+                    output += board[posCorrespondences[i]];
+                    if (board[posCorrespondences[i]] == null)
+                    {
+                        output += Actions.Any(o => o.Position == posCorrespondences[i]) ? "!" : " ";
+                    }
+                }
+                else
+                {
+                    switch (posCorrespondences[i])
+                    {
+                        case -1: output += "|"; break;
+                        case -2: output += "-"; break;
+                        case -3: output += "+"; break;
+                        case -4: output += "\n"; break;
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
